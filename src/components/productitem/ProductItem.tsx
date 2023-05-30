cec91b699eef00cd86b64d10a39f8a25792c9b14
knopgm <knop.gm@gmail.com>
2023-05-30 23:42:12 +0200

    Improve product cart layout, add buttons to add and remove product from the shop cart

--- src/components/productitem/ProductItem.tsx
+++ src/components/productitem/ProductItem.tsx
@@ -10,20 +10,30 @@ export const ProductItem = ({ product }: ProductItemProps) => {
   const { id, title, price } = product;
   const { value, currency } = price;
   const productImage = require("../../img/shoe2.png");
+  const cartImage = require("../../img/orange_cart.png");
 
   return (
     <>
       <div className="product_item-wrapper" id={`${id}`}>
-        <div>
+        <div className="img_frame">
           <img
             alt="product image"
             style={{ width: 100 }}
             src={String(productImage)}
           />
         </div>
-        <div>{title}</div>
-        <div>
+        <h2>{title}</h2>
+        <p>
           {currency} {value}
+        </p>
+        <div className="add_remove_cart-wrapper">
+          <button className="button-add_remove">-</button>
+          <img
+            alt="cart image"
+            style={{ width: 40, padding: 3 }}
+            src={String(cartImage)}
+          />
+          <button className="button-add_remove">+</button>
         </div>
       </div>
     </>
